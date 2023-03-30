2e9dcce7612c34e32c6b709bb733a0a9b92bf31f
Artur <krolczyk.artur@gmail.com>
2023-03-30 09:01:57 +0200

    next

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
 
   images: {
     domains: ['cdn.pixabay.com', 'cdn-images-1.medium.com'],
