99e296fba0a90d0a358546fab589efc2a6bd1e37
Artur <krolczyk.artur@gmail.com>
2023-03-30 07:35:51 +0200

    poc rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // rewrites: async () => [
+  //   {
+  //     source: '/backstage/:slug*',
+  //     destination: 'https://sso-roland.vercel.app/:slug*',
+  //   },
+  // ],
   images: {
     domains: ['cdn.pixabay.com', 'cdn-images-1.medium.com'],
   },
