1d3efa90380ddf24da84ae51a539cb6fff94f948
Artur <krolczyk.artur@gmail.com>
2023-03-30 08:04:10 +0200

    used before files

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // rewrites: async () => [
-  //   {
-  //     source: '/backstage/:slug*',
-  //     destination: 'https://sso-roland.vercel.app/:slug*',
-  //   },
-  // ],
+  rewrites: async () => ({
+    beforeFiles: [
+      {
+        source: '/backstage/:slug*',
+        destination: 'https://sso-roland.vercel.app/:slug*',
+      },
+    ],
+  }),
   images: {
     domains: ['cdn.pixabay.com', 'cdn-images-1.medium.com'],
   },
