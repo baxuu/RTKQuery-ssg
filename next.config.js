61ab8c06c536d282cf62470aa279e723374814dd
Artur <krolczyk.artur@gmail.com>
2023-03-30 11:27:58 +0200

    next

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
 
+  rewrites: async () => [
+    {
+      source: '/backstage/:slug*',
+      destination: 'https://sso-roland.vercel.app/:slug*',
+    },
+    {
+      source: '/_nuxt/:slug*',
+      destination: 'https://sso-roland.vercel.app/_nuxt/:slug*',
+    },
+  ],
   images: {
     domains: ['cdn.pixabay.com', 'cdn-images-1.medium.com'],
   },
